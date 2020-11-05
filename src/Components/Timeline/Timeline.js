76b7079a458a5dc090b35a8598a43fa26af66134
salt-dev <konrad.guzior@gmail.com>
2020-11-05 17:25:59 +0100

    Added favicon, changed data order

--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {useSelector} from 'react-redux'
 import {selectData} from '../../store/AppSlice'
 import Timeline from '@material-ui/lab/Timeline';
+import LaptopMacIcon from '@material-ui/icons/LaptopMac';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineDot from '@material-ui/lab/TimelineDot';
 
 import TimelineItem from './TimelineItem'
 
